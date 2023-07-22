b2a9aa2e63786e52ba74f8d92b9ed036f29a0094
Anish949 <74363280+Anish949@users.noreply.github.com>
2023-07-22 11:15:37 +0545

    keyboard accessibility to search

--- onlinestore/src/components/Navbar.jsx
+++ onlinestore/src/components/Navbar.jsx
@@ -1,12 +1,19 @@
 import Image from "next/image";
 import React, { useState } from "react";
 import Link from "next/link";
+import { useRef } from "react";
 const Navbar = () => {
+  let searchIcon = useRef(null);
   const [query, setQuery] = useState("");
 
   // for handling change in input and updating the state
   const handleInputChange = (e) => {
     setQuery(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    searchIcon.current.click();
   };
   return (
     <div className="w-full bg-purple-600 flex p-4 gap-16 max flex-col align-center sm:flex-row">
@@ -37,40 +44,43 @@ const Navbar = () => {
       </ul>
       {/* Search Bar */}
 
-      <div className="searchBar w-full flex gap-2 items-center">
-        <input
-          type="search"
-          onChange={handleInputChange}
-          id="search"
-          className="block ml-auto self-end max-w-40 p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
-          placeholder="Search Tshirts, Smartphones..."
-        />
-        {/* search icon
-         */}
-        <Link
-          href={{
-            pathname: "/products",
-            query: { search: query },
-          }}
-          className="w-12 h-12 hover:bg-black rounded-lg flex justify-center items-center border-yellow-300 border hover:bg-transparent transition-all"
-        >
-          <svg
-            className="w-8 h-8 text-white "
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 20 20"
+      <form action="#" className="inline-block ml-auto" onSubmit={handleSubmit}>
+        <div className="searchBar w-full flex gap-2 items-center">
+          <input
+            type="search"
+            onChange={handleInputChange}
+            id="search"
+            className="block ml-auto self-end max-w-40 p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
+            placeholder="Search Shirt,gold..."
+          />
+          {/* search icon
+           */}
+          <Link
+            ref={searchIcon}
+            href={{
+              pathname: "/products",
+              query: { search: query },
+            }}
+            className="w-12 h-12 hover:bg-black rounded-lg flex justify-center items-center border-yellow-300 border hover:bg-transparent transition-all"
           >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="3"
-              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-            />
-          </svg>
-        </Link>
-      </div>
+            <svg
+              className="w-8 h-8 text-white "
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 20"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="3"
+                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+              />
+            </svg>
+          </Link>
+        </div>
+      </form>
     </div>
   );
 };
